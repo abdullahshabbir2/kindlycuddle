8514efd76d844716074d64f633d8bf3b5110daab
Ham-lan <hamzasardar954@gmail.com>
2024-12-25 07:42:36 +0500

    Shared Preference removed for now

--- lib/Service/Shared Preference Service/shared_preference_service.dart
+++ lib/Service/Shared Preference Service/shared_preference_service.dart
@@ -2,16 +2,20 @@ import 'package:cuddle_care/Domain/Entity/pulse_weight.dart';
 import 'package:cuddle_care/Domain/Failure/get_pulse_weight_failure.dart';
 import 'package:cuddle_care/Domain/Failure/set_pulse_weight_failure.dart';
 import 'package:dartz/dartz.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferenceService{
   Future<Either<GetPulseWeightFailure, PulseWeight>> getPulseWeight() async {
     try{
-      final prefs = await SharedPreferences.getInstance();
+      // final prefs = await SharedPreferences.getInstance();
 
       // Get an integer
-      double? pulse = prefs.getDouble('pulse') ?? 0;
-      double? weight = prefs.getDouble('weight') ?? 0;
+      double? pulse =
+          // prefs.getDouble('pulse') ??
+          0;
+      double? weight =
+          // prefs.getDouble('weight') ??
+               0;
 
       PulseWeight data = PulseWeight(pulse: pulse , weight:  weight);
 
@@ -23,9 +27,9 @@ class SharedPreferenceService{
 
   Future<Either<SetPulseWeightFailure, bool>> setPulseWeight(double pulse, double weight) async {
     try{
-      final prefs = await SharedPreferences.getInstance();
-      prefs.setDouble('pulse', pulse);
-      prefs.setDouble('weight', weight);
+      // final prefs = await SharedPreferences.getInstance();
+      // prefs.setDouble('pulse', pulse);
+      // prefs.setDouble('weight', weight);
 
       return right(true);
 
