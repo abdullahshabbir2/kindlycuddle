537ee83778b0ba4c7160ce9505a72ec26d62dc8d
kbone1997 <colin@koenig.us>
2025-01-10 04:11:10 +0600

    updated home screen ui

--- lib/UI/ReUseAble/bottom_navigator.dart
+++ lib/UI/ReUseAble/bottom_navigator.dart
@@ -1,6 +1,7 @@
 import 'package:cuddle_care/Constants/image_constants.dart';
 import 'package:cuddle_care/UI/ReUseAble/re_use_able_svg.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyBottomNavigator extends StatefulWidget {
   final int index;
@@ -17,48 +18,44 @@ class _MyBottomNavigatorState extends State<MyBottomNavigator> {
   Widget build(BuildContext context) {
     return Visibility(
       visible: widget.index != 2,
-      child: Container(
-        decoration: BoxDecoration(
-            border: Border.all(
-          color: Colors.black,
-          width: 1.0,
-        )),
-        child: Stack(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * (90 / 712),
+      child: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * (90 / 712),
+            width: MediaQuery.of(context).size.width * (343 / 375),
+            child: SvgPicture.asset(
+              ImageConstants.bottomRectangle,
               width: MediaQuery.of(context).size.width * (343 / 375),
-              child: ReUseAbleSvg(
-                path: ImageConstants.bottomRectangle,
-                width: MediaQuery.of(context).size.width * (343 / 375),
-                height: MediaQuery.of(context).size.height * (102 / 812),
-              ),
+              height: MediaQuery.of(context).size.height * (102 / 512),
             ),
-            Container(
-              height: MediaQuery.of(context).size.height * (90 / 812),
-              width: MediaQuery.of(context).size.width * (343 / 375),
-              padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  MyNavBarItem(
-                      path: widget.index == 0
-                          ? ImageConstants.home_2
-                          : ImageConstants.home2,
-                      onTap: () {
-                        widget.onTap(0);
-                      }),
-                  MyNavBarItem(
-                      path: widget.index == 1
-                          ? ImageConstants.pieChart_1
-                          : ImageConstants.pieChart2,
-                      onTap: () {
-                        widget.onTap(1);
-                      }),
-                  InkWell(
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * (90 / 812),
+            width: MediaQuery.of(context).size.width * (343 / 375),
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                MyNavBarItem(
+                    path: widget.index == 0
+                        ? ImageConstants.home_2
+                        : ImageConstants.home2,
                     onTap: () {
-                      widget.onTap(2);
-                    },
+                      widget.onTap(0);
+                    }),
+                MyNavBarItem(
+                    path: widget.index == 1
+                        ? ImageConstants.pieChart_1
+                        : ImageConstants.pieChart2,
+                    onTap: () {
+                      widget.onTap(1);
+                    }),
+                InkWell(
+                  onTap: () {
+                    widget.onTap(2);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 5),
                     child: Column(
                       children: [
                         SizedBox(
@@ -73,23 +70,23 @@ class _MyBottomNavigatorState extends State<MyBottomNavigator> {
                       ],
                     ),
                   ),
-                  MyNavBarItem(
-                      path: ImageConstants.book2,
-                      onTap: () {
-                        widget.onTap(3);
-                      }),
-                  MyNavBarItem(
-                      path: widget.index == 4
-                          ? ImageConstants.setting_1
-                          : ImageConstants.setting,
-                      onTap: () {
-                        widget.onTap(4);
-                      }),
-                ],
-              ),
+                ),
+                MyNavBarItem(
+                    path: ImageConstants.book2,
+                    onTap: () {
+                      widget.onTap(3);
+                    }),
+                MyNavBarItem(
+                    path: widget.index == 4
+                        ? ImageConstants.setting_1
+                        : ImageConstants.setting,
+                    onTap: () {
+                      widget.onTap(4);
+                    }),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -122,8 +119,8 @@ class MyNavBarItem extends StatelessWidget {
           ),
           ReUseAbleSvg(
             path: path,
-            height: height ?? 24,
-            width: width ?? 24,
+            height: height ?? 28,
+            width: width ?? 28,
           ),
         ],
       ),
